b9c1463aaac114324e4e42493d11757e256dd899
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-22 22:18:13 +0200

    Add module example

---
+++ 04-classes/05-modules/Customer.ts
@@ -0,0 +1,24 @@
+// export class as module
+export class Customer {
+    // constructor using parameter property
+    constructor(private _firstName: string, private _lastName : string) {
+
+    }
+
+    // get/set accessor are only available ES5
+    public get firstName(): string {
+        return this._firstName;
+    }
+    
+    // no return, void need not to be specified
+    public set firstName(value: string) {
+        this._firstName = value;
+    }
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+}
