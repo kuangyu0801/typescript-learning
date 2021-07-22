dcd0d085a8dc80f30aae431a5ff8f15249cb7b3f
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2021-07-22 22:04:12 +0200

    Add classes example

---
+++ 04-classes/02-class-modifier/Customer.ts
@@ -0,0 +1,34 @@
+// tsc --target ES5 --noEmitOnError
+// tsc --init
+class Customer {
+    // default accessor is public
+    private _firstName: string;
+    private _lastName: string;
+
+    public get lastName(): string {
+        return this._lastName;
+    }
+    public set lastName(value: string) {
+        this._lastName = value;
+    }
+
+    constructor(theFirst : string, theLast) {
+        this._firstName = theFirst;
+        this._lastName = theLast;
+    }
+
+    // get/set accessor are only available ES5
+    public get firstName(): string {
+        return this._firstName;
+    }
+    
+    // no return, void need not to be specified
+    public set firstName(value: string) {
+        this._firstName = value;
+    }
+}
+
+let myCustomer = new Customer("Martin", "Dixon");
+
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
